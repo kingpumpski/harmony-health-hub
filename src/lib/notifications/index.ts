a96452856f5b46a433fe5b7f9024579fdbd917b7
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-09-25 14:48:51 +0000

    feat(notifications): add channel-agnostic notification contracts

---
+++ src/lib/notifications/index.ts
@@ -0,0 +1,6 @@
+export * from './contracts';
+export * from './dispatcher';
+export * from './preferences';
+export * from './scheduler';
+export * from './templates';
+export * from './featureFlags';
